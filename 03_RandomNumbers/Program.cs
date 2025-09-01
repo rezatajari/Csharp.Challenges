51092a2a9404267fb2852a29a823eb275ad6d082
rezatajari <reza.tajari70@gmail.com>
2025-09-01 13:21:40 +0330

    feat: Add 03_RandomNumbers project to solution
    Introduced a new console project that generates 1000 random numbers, tracks the max and min odd values, and counts the total odd numbers generated. Updated the solution file to include the new project.

---
+++ 03_RandomNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int max=0;
+int min=10000;
+int counter = 0;
+int randomNumber;
+
+Random rand = new Random();
+
+for (int i = 0; i < 1000; i++)
+{
+    randomNumber=rand.Next(0,100001);
+    if (randomNumber % 2 != 0)
+    {
+        counter++;
+        max = (randomNumber > max) ? randomNumber : max;
+        min = (randomNumber < min) ? randomNumber : min;
+    }
+}
+
+Console.WriteLine("Max Number: {0}\n" +
+                  "Min Number: {1}\n" +
+                  "Total Number Generated: {2} Final Result",max,min, counter);
+
+
