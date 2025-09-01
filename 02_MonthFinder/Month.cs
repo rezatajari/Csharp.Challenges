c18fff899c707de50b1e8cacbf929f3442f914a2
rezatajari <reza.tajari70@gmail.com>
2025-09-01 10:39:40 +0330

    feat: Add MonthFinder project to solution
    Introduced the 02_MonthFinder project with Month and Program classes. The Month class provides month name and days based on input, and Program.cs implements a simple console interface. Updated the solution file to include the new project.

---
+++ 02_MonthFinder/Month.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_MonthFinder;
+
+public class Month
+{
+    public Month(int monthNumber)
+    {
+        MonthNumber = monthNumber;
+    }
+
+    public int  MonthNumber { get; set; }
+
+    public string GetMonthName()
+    {
+        string monthName = "";
+        switch (MonthNumber)
+        {
+            case 1:
+                monthName = "January";
+                break;
+            case 2:
+                monthName = "February";
+                break;
+            case 3:
+                monthName = "March";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "May";
+                break;
+            case 6:
+                monthName = "June";
+                break;
+            case 7:
+                monthName = "July";
+                break;
+            case 8:
+                monthName = "August";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "October";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "December";
+                break;
+            default:
+                monthName = "Invalid Input";
+                break;
+        }
+
+        return monthName;
+    }
+
+    public int GetMonthDays()
+    {
+        if (MonthNumber == 1 || MonthNumber == 3 || MonthNumber == 5 || MonthNumber == 7 || MonthNumber == 8 ||
+            MonthNumber == 10 || MonthNumber == 12)
+            return 31;
+        else if (MonthNumber == 4 || MonthNumber == 6 || MonthNumber == 9 || MonthNumber == 11)
+            return 30;
+        else if (MonthNumber == 2)
+            return 28;
+        else
+            return 0;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Month of {0} has {1} days", GetMonthName(), GetMonthDays());
+    }
+}
---
+++ 02_MonthFinder/Program.cs
@@ -0,0 +1,8 @@
+﻿using _02_MonthFinder;
+
+Console.WriteLine("Please enter the month number: ");
+int monthNumber=Convert.ToInt32(Console.ReadLine());
+
+Month month=new Month(monthNumber);
+
+Console.WriteLine(month);
