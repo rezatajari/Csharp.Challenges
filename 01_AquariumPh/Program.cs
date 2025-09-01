54a555fffc34aab41f50418e82581bbfd4fe0bfe
rezatajari <reza.tajari70@gmail.com>
2025-09-01 09:51:41 +0330

    feat: Add pH level input and classification logic
    Program now prompts the user for a pH value, validates the input, and classifies it as acidic, alkaline, or neutral based on the entered value.

--- 01_AquariumPh/Program.cs
+++ 01_AquariumPh/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int ph;
+Console.WriteLine("Please enter the pH level: ");
+if (int.TryParse(Console.ReadLine(),out ph)==false||ph<0||ph>14)
+    Console.WriteLine("Invalid Input");
+else if (ph<7)
+    Console.WriteLine("pH is Acidic");
+else if (ph>7)
+    Console.WriteLine("pH is Alkaline");
+else
+    Console.WriteLine("pH is neutral");
