65caa151662b6f93f6551545a08e043b703864db
rezatajari <reza.tajari70@gmail.com>
2025-09-04 09:22:52 +0330

    feat: Add ValueFrequencyAnalyzer project
    Introduces the 06_ValueFrequencyAnalyzer project with logic to analyze frequency of integer values in an array. Includes project file, main program, and TestArray class for counting valid entries and their occurrences. Updates solution file to include the new project.

---
+++ 06_ValueFrequencyAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using _06_ValueFrequencyAnalyzer;
+
+TestArray arr=new TestArray();
+Console.WriteLine("Number of elements: {0}",arr.NumberOfElements.ToString());
+Console.WriteLine("Valid entries: {0}", arr.ValidEntries());
+Console.WriteLine("Invalid entries: {0}",arr.NumberOfElements - arr.ValidEntries());
+
+int[] entriesCount=arr.CountEntries();
+for (int i = 0; i < entriesCount.Length; i++)
+{
+        Console.WriteLine("Number of occurances for value of {0} is {1} ", i, entriesCount[i]);
+}
---
+++ 06_ValueFrequencyAnalyzer/TestArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ValueFrequencyAnalyzer;
+
+internal class TestArray
+{
+    private int[] numbers = { 1, 7, 2,11, 4, 2, 3, 8, 4, 6, 4, 4, 7 };
+
+    public TestArray()
+    {
+        NumberOfElements = numbers.Length;
+    }
+
+    public int NumberOfElements { get; set; }
+
+    public int ValidEntries()
+    {
+        int counter = 0;
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            if (numbers[i] >= 0 && numbers[i] <= 10)
+                counter++;
+        }
+
+        return counter;
+    }
+
+    public int[] CountEntries()
+    {
+        int[] numbers2 = new int[11];
+
+        for (int i = 0; i < NumberOfElements; i++)
+        {
+            if (numbers[i] >= 0 && numbers[i] <= 10)
+                numbers2[numbers[i]]++;
+        }
+
+        return numbers2;
+    }
+}
