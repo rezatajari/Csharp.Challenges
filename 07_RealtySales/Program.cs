c51e8b955f7e2b261c707a2ee8c257e5ed7bb08b
rezatajari <reza.tajari70@gmail.com>
2025-09-04 19:19:49 +0330

    feat: Add 07_RealtySales project for sales analysis
    Introduces the 07_RealtySales console application, which collects realty sales data, calculates each sale's contribution to the total, and displays summary statistics. Updates the solution file to include the new project.

---
+++ 07_RealtySales/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberOfSales=0;
+double[] sales;
+
+Console.Write("Please enter number of sales ");
+numberOfSales=Convert.ToInt32(Console.ReadLine());
+
+sales=new double[numberOfSales];
+
+for (int i = 0; i < numberOfSales; i++)
+{
+    Console.Write("Please enter sale #{0}: ",i+1);
+    sales[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+double sum=sales.Sum();
+for (int i = 0; i < numberOfSales; i++)
+{
+    double contribution = sales[i] / sum;
+    Console.WriteLine("Sale # {0} was {1:C2} and contributed {2:P2}", i + 1, sales[i], contribution);
+}
+Console.WriteLine("Total sum of sales is {0:C2}",sum);
