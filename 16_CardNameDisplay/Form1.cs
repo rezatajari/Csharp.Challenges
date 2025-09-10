840bd902d68fdb17f867eb8e2b103d1f94fbec83
rezatajari <reza.tajari70@gmail.com>
2025-09-10 08:25:05 +0330

    feat: implement card name display Windows Form & readme
    - Added 5 PictureBox controls for different card images
    - Added a Label to display the card name
    - Click events update the Label with the name of the clicked card
    - Demonstrates interactive WinForms UI and event handling

---
+++ 16_CardNameDisplay/Form1.cs
@@ -0,0 +1,54 @@
+﻿/*
+
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16_CardNameDisplay
+{
+  public partial class frmCardIdentifier : Form
+  {
+    public frmCardIdentifier()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void pictureBox1_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "2 of Clubs";
+      lblOutput.Text = "2 of Clubs";
+        }
+
+    private void pic2_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "2 of Diamonds";
+    }
+
+    private void pic3_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "2 of Hearts";
+    }
+
+    private void pic4_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "2 of Spades";
+    }
+
+    private void pic5_Click(object sender, EventArgs e)
+    {
+      lblOutput.Text = "Joker";
+    }
+  }
+}
