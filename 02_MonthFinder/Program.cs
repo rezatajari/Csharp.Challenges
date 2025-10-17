297bb9e5e90f66f8ad148296ff38b65a7383f935
rezatajari <reza.tajari70@gmail.com>
2025-10-17 18:44:05 +0330

    chore: folder structure seperate for each one

--- 02_MonthFinder/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using _02_MonthFinder;
-
-Console.WriteLine("Please enter the month number: ");
-int monthNumber=Convert.ToInt32(Console.ReadLine());
-
-Month month=new Month(monthNumber);
-
-Console.WriteLine(month);
