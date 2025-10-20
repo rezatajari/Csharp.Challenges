17dea294d787e51095f695f83f99507a28611e8c
rezatajari <reza.tajari70@gmail.com>
2025-10-20 16:57:55 +0330

    feat: lasagna preparation

---
+++ exercism/Lucian'sLusciousLasagna/Lasagna.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lucian_sLusciousLasagna
+{
+    internal class Lasagna
+    {
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+
+        public int RemainingMinutesInOven(int actualMinutes)
+        {
+            return ExpectedMinutesInOven() - actualMinutes;
+        }
+
+        public int PreparationTimeInMinutes(int numberOfLayers)
+        {
+            return numberOfLayers * 2;
+        }
+
+        public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutes)
+        {
+            int timeLayers = PreparationTimeInMinutes(numberOfLayers);
+            return timeLayers + actualMinutes;
+        }
+    }
+}
---
+++ exercism/Lucian'sLusciousLasagna/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lucian_sLusciousLasagna;
+
+var lasagna = new Lasagna();
+
+Console.WriteLine("Testing Lasagna class...");
+Console.WriteLine("-----------------------------");
+
+Console.WriteLine($"Expected minutes in oven: {lasagna.ExpectedMinutesInOven()}");
+Console.WriteLine($"Remaining minutes after 30: {lasagna.RemainingMinutesInOven(30)}");
+Console.WriteLine($"Preparation time for 2 layers: {lasagna.PreparationTimeInMinutes(2)}");
+Console.WriteLine($"Elapsed time (3 layers, 20 minutes in oven): {lasagna.ElapsedTimeInMinutes(3, 20)}");
