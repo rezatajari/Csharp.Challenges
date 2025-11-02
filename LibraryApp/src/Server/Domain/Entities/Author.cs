2ddf03bffc9caeb5d5100a6db42f6a6c5af8c8e7
rezatajari <reza.tajari70@gmail.com>
2025-11-02 17:05:37 +0330

    feat: author,book,bookauthor

---
+++ LibraryApp/src/Server/Domain/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? About { get; set; }
+        public string? Country { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public ICollection<BookAuthor> BookAuthors { get; set; } = new List<BookAuthor>();
+    }
+}
---
+++ LibraryApp/src/Server/Domain/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public int? PublishedYear { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public ICollection<BookAuthor> BookAuthors { get; set; } = new List<BookAuthor>();
+    }
+}
