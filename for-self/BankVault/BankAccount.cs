14f96f0df4ac231092fad8c23870d8a1b0cd1c30
rezatajari <reza.tajari70@gmail.com>
2026-01-01 09:21:19 +0330

    feat: bank system (Encapsulation)

---
+++ for-self/BankVault/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankVault
+{
+    internal class BankAccount
+    {
+        private decimal _money = 0;
+        public BankAccount(decimal amount)
+        {
+            AddMoney(amount);
+        }
+        public void AddMoney(decimal amount)
+        {
+            if (amount >= 0)
+                _money += amount;
+        }
+        public void RemoveMoney(decimal amount)
+        {
+            if (amount > 0 && _money >= amount)
+                _money -= amount;
+        }
+    }
+}
