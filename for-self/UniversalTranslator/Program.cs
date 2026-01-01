d18238819fed01ea4a7cd9d173593af006aaebaf
rezatajari <reza.tajari70@gmail.com>
2026-01-01 12:41:49 +0330

    feat: universal translator (generic)

---
+++ for-self/UniversalTranslator/Program.cs
@@ -0,0 +1,11 @@
+﻿var translator = new UniversalTranslator.UniversalTranslator();
+
+int intResult = translator.DoubleInput<int>(2);
+
+decimal decimalResult = translator.DoubleInput<decimal>(3.2m);
+
+double doubleResult = translator.DoubleInput<double>(4.2);
+
+Console.WriteLine($"int result: {intResult}");
+Console.WriteLine($"decimal result: {decimalResult}");
+Console.WriteLine($"double result: {doubleResult}");
---
+++ for-self/UniversalTranslator/UniversalTranslator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace UniversalTranslator
+{
+    public class UniversalTranslator
+    {
+       
+        public T DoubleInput<T>(T numericInput)
+            where T : INumber<T>
+        {
+            T multiplier = T.CreateChecked(2);
+
+            return numericInput * multiplier;
+        }
+    }
+
+}
