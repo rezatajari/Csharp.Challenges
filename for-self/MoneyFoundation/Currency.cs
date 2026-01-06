d818ddcca9ca9820de9c3c8c4ec48dfeb144e0b0
rezatajari <reza.tajari70@gmail.com>
2026-01-06 09:11:23 +0330

    refactor: fix body struct

--- for-self/MoneyFoundation/Currency.cs
+++ for-self/MoneyFoundation/Currency.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace MoneyFoundation
 {
-    internal class Currency
+    public readonly struct Currency(string name, decimal rate)
     {
+        public string Name { get; } = string.IsNullOrEmpty(name)
+            ? throw new NullReferenceException("The name is required")
+            : name;
+        public decimal Rate { get; } = rate > 0
+            ? rate
+            : throw new ArgumentOutOfRangeException(nameof(rate), "Rate must be positive.");
     }
 }
