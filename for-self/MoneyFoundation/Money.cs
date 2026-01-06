fe16fb0b834d5f8c43afbaa068570ba58b6bf2ac
rezatajari <reza.tajari70@gmail.com>
2026-01-06 18:30:44 +0330

    feat: add operation pelas for Money

--- for-self/MoneyFoundation/Money.cs
+++ for-self/MoneyFoundation/Money.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MoneyFoundation
+﻿namespace MoneyFoundation
 {
-    public class Money(decimal amount, Currency currency)
+    public readonly struct Money(decimal amount, Currency currency)
     {
-        public decimal Amount { get; set; } = amount;
-        public Currency Currency { get; set; } = currency;
+        public decimal Amount { get;} = amount;
+        public Currency Currency { get;  } = currency;
+
+        public static Money operator +(Money left, Money right)
+        => (left.Currency != right.Currency)
+            ? throw new InvalidOperationException(
+                $"Mismatched Currency: Cannot add {left.Currency.Name} and {right.Currency.Name}")
+            : new Money(left.Amount + right.Amount, left.Currency);
     }
 }
