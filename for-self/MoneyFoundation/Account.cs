b6eeb72bc92fdec3ff9c622ec7af4368bc4fabe0
rezatajari <reza.tajari70@gmail.com>
2026-01-07 10:55:03 +0330

    feat: implement Account entity with guarded constructor and dependency injection for Currency

---
+++ for-self/MoneyFoundation/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyFoundation
+{
+    public class Account
+    {
+        private static int counter = 0;
+        public int Number { get; init; }
+        public User User { get; set; }
+        public Currency Currency { get; set; }
+        public Money Balance { get; private set; }
+        public Account(User user, Currency currency)
+        {
+            Number = new Random().Next(100, 199) + (++counter);
+            User = user ?? throw new ArgumentNullException(nameof(user), "User cannot be null");
+
+            if (string.IsNullOrEmpty(currency.Name))
+                throw new ArgumentException("Invalid Currency provided.", nameof(currency));
+
+            Currency = currency;
+            Balance = new Money(0, Currency);
+        }
+    }
+}
---
+++ for-self/MoneyFoundation/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyFoundation
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+    }
+}
