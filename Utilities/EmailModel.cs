c64a72a57e1eea95263f5d80a832188c076e82ab
rezatajari <reza.tajari70@gmail.com>
2026-02-02 09:50:09 +0330

    refactor: redesign

--- Utilities/EmailModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibraryAppMVC.Utilities
-{
-    public class EmailModel
-    {
-        public string Email { get; set; }
-        public string Subject { get; set; }
-        public string Message { get; set; }
-    }
-}
--- ViewModels/ListBookViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LibraryAppMVC.ViewModels
-{
-    public class ListBookViewModel
-    {
-        public List<BookViewModel>? BookListViewMode { get; set; }
-    }
-}
