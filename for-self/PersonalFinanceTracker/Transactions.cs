985715957f358e9fc122cf12289af94572d5381e
rezatajari <reza.tajari70@gmail.com>
2026-02-26 13:09:02 +0330

    feat: add transactions record model

---
+++ for-self/PersonalFinanceTracker/Transactions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalFinanceTracker
+{
+    public record Transactions (int Id,decimal Amount,string Description, DateTime Date);
+}
