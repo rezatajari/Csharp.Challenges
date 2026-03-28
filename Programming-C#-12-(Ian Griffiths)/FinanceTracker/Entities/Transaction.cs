1145b87c6418a56a9fe5d90166ef621709055a54
rezatajari <reza.tajari70@gmail.com>
2026-03-28 10:15:22 +0330

    refactor: validation and factory method

--- Programming-C#-12-(Ian Griffiths)/FinanceTracker/Entities/Transaction.cs
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Entities/Transaction.cs
@@ -6,10 +6,36 @@ namespace FinanceTracker.Entities
 {
     public class Transaction
     {
-        public decimal Amount { get; set; }
-        public DateTime CreateAt{ get; set; }
-        public Category Category{ get; set; }
-        public Account Account{ get; set; }
-        public string? Description { get; set; }
+        private Transaction(decimal amount, Category category,
+            Account account, string? description)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount must be greater than zero.", nameof(amount));
+            }
+            this.Amount = amount;
+
+            if (category==null || account==null)
+            {
+                throw new ArgumentNullException("Category and Account cannot be null.");
+            }
+            this.Category = category;
+            this.Account = account;
+
+            this.Description = description;
+            this.CreateAt = DateTime.Now;
+        }
+        public decimal Amount { get; private set; }
+        public DateTime CreateAt { get; private set; }
+        public Category Category { get; private set; }
+        public Account Account { get; private set; }
+        public string? Description { get; private set; }
+
+        public static Transaction CreateTransaction(decimal amount, Category category,
+            Account account, string? description)
+        {
+            return new Transaction(amount, category, account, description);
+        }
+
     }
 }
