d898e0b4bec9c7485f6c394bb675b33635b573d2
rezatajari <reza.tajari70@gmail.com>
2026-03-28 09:22:12 +0330

    feat: Add FinanceTracker entity classes; remove Program.cs
    Introduce domain entities for the FinanceTracker app: add Account and Category placeholder classes and a Transaction class in FinanceTracker.Entities. Transaction includes Amount (decimal), CreateAt (DateTime), Category and Account references, and a nullable Description string. Remove an empty Program.cs file.

---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Entities/Account.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Entities
+{
+    public class Account
+    {
+    }
+}
---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Entities/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Entities
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public DateTime CreateAt{ get; set; }
+        public Category Category{ get; set; }
+        public Account Account{ get; set; }
+        public string? Description { get; set; }
+    }
+}
