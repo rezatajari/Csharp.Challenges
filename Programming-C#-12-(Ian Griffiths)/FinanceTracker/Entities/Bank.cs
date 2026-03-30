b5699f99d29d2497e499da789dd3496f31014879
rezatajari <reza.tajari70@gmail.com>
2026-03-30 12:07:14 +0330

    feat: bank & GetTotalNetWorths method in it

---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Entities/Bank.cs
@@ -0,0 +1,34 @@
+﻿using FinanceTracker.Interfaces;
+using FinanceTracker.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Entities
+{
+    public class Bank
+    {
+        private Bank()
+        {
+
+        }
+
+        private readonly List<IAccount> _accounts = [];
+
+        public IEnumerable<IAccount> Accounts => _accounts.AsReadOnly();
+
+        public void AddAccount(IAccount account)
+        {
+            _accounts.Add(account);
+        }
+
+        public List<Money> GetTotalNetWorths()
+        {
+            return _accounts.GroupBy(acc => acc.Balance.Currency)
+                .Select(group => Money.Create(
+                    group.Sum(a => a.Balance.Amount),
+                    group.Key))
+                .ToList();
+        }
+    }
+}
---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Interfaces/ICurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Interfaces
+{
+    public interface ICurrencyConverter
+    {
+    }
+}
