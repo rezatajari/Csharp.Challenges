41e5b255f001442888df3a69fdf67b686acf3d38
rezatajari <reza.tajari70@gmail.com>
2026-03-30 10:22:56 +0330

    feat: exception traning

--- Programming-C#-12-(Ian Griffiths)/FinanceTracker/Program.cs
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using FinanceTracker.Entities;
+
+var myAccount = Account.Create("Savings", Money.Create(100, Currency.USD), TypeName.Bank);
+var targetAccount = Account.Create("Checking", Money.Create(0, Currency.USD), TypeName.Bank);
+
+try
+{
+    myAccount.TransferTo(targetAccount, Money.Create(500, Currency.USD), DateTime.Now, description: "Transfer to other my account");
+    Console.WriteLine("Success!");
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("User Error: " + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("System Error: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Session closed. Current Balance: " + myAccount.Balance.Amount);
+}
