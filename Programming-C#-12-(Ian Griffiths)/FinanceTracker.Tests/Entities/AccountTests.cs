7907cea3540b450b81767ddc7f7d1dbc8cb2f38b
rezatajari <reza.tajari70@gmail.com>
2026-04-05 15:43:00 +0330

    feat: Account_Should_SetInitialBalance_WhenCreated test

---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker.Tests/Entities/AccountTests.cs
@@ -0,0 +1,22 @@
+﻿using FinanceTracker.Entities;
+using FinanceTracker.ValueObjects;
+
+namespace FinanceTracker.Tests.Entities
+{
+    public class AccountTests
+    {
+        [Fact]
+        public void Account_Should_SetInitialBalance_WhenCreated()
+        {
+            // Arrange
+            var initialAmount=Money.Create(100, Currency.USD);
+
+            // Act
+            var account = Account.Create("My Savings", initialAmount, TypeName.Bank);
+
+            // Assert
+            Assert.Equal(100, account.Balance.Amount);
+
+        }
+    }
+}
---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker.Tests/Services/FinanceServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Tests.Services
+{
+    internal class FinanceServiceTests
+    {
+    }
+}
