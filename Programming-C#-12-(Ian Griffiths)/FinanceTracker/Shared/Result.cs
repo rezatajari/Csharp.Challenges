4351a363a9ea62cfddec99956c39e932060da376
rezatajari <reza.tajari70@gmail.com>
2026-04-05 12:27:08 +0330

    feat: add Result for general response as structure

---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Shared/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Shared
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public string ErrorMessage { get; }
+        
+        protected Result(bool success,T? value,string errorMessage)
+        {
+            IsSuccess = success;
+            Value = value;
+            ErrorMessage = errorMessage;
+        }
+
+        public static Result<T> Success(T value) => new(true, value, string.Empty);
+        public static Result<T> Failure(string errorMessage) => new(false, default, errorMessage);
+    }
+}
