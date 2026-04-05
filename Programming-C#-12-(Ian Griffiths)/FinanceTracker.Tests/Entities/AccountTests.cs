41f302c02b6764684e58f1a09ff69a377e98b429
rezatajari <reza.tajari70@gmail.com>
2026-04-05 15:52:45 +0330

    feat: Deposit_Should_IncreaseBalance_WhenAmountIsPositive test

--- Programming-C#-12-(Ian Griffiths)/FinanceTracker.Tests/Entities/AccountTests.cs
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker.Tests/Entities/AccountTests.cs
@@ -9,14 +9,25 @@ namespace FinanceTracker.Tests.Entities
         public void Account_Should_SetInitialBalance_WhenCreated()
         {
             // Arrange
-            var initialAmount=Money.Create(100, Currency.USD);
+            var initialAmount = Money.Create(100, Currency.USD);
 
             // Act
             var account = Account.Create("My Savings", initialAmount, TypeName.Bank);
 
             // Assert
             Assert.Equal(100, account.Balance.Amount);
+        }
 
+        [Fact]
+        public void Deposit_Should_IncreaseBalance_WhenAmountIsPositive()
+        {
+            var initialAmount = Money.Create(100, Currency.USD);
+            var depositAmount = Money.Create(50, Currency.USD);
+            var account = Account.Create("My Savings", initialAmount, TypeName.Bank);
+
+            account.Deposit(depositAmount, Category.Create("Income", null, TransactionType.Income), null, DateTime.Now);
+
+            Assert.Equal(150, account.Balance.Amount);
         }
     }
 }
