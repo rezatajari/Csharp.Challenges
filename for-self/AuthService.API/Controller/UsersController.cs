aa13b40e9730ae8f520a056180ca2360da425e1f
rezatajari <reza.tajari70@gmail.com>
2026-04-09 11:54:20 +0330

    refactor: static for singleton concept

--- for-self/AuthService.API/Controller/UsersController.cs
+++ for-self/AuthService.API/Controller/UsersController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AuthService.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthService.API.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class UsersController : ControllerBase
     {
+        private static  List<User> users=new List<User>();
+
+        public async Task<IActionResult> Registration(User user)
+        {
+            if (users.Any(u=>u.Email==user.Email))
+            {
+                return BadRequest("Email already exists");
+            }
+
+            users.Add(user);
+            return Ok("User registered successfully");
+        }
     }
 }
