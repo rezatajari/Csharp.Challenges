42c1af834bbae0074a260f04b0601f4b18036be9
rezatajari <reza.tajari70@gmail.com>
2026-04-09 10:37:51 +0330

    feat: create user

---
+++ for-self/AuthService.API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace AuthService.API.Models
+{
+    public class User
+    {
+        private User(string username,string password)
+        {
+            Username=username;
+            Password=password;
+        }
+
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+
+
+        public static User Create(string username,string password)
+        {
+            return new User(username,password);
+        }
+    }
+}
--- for-self/AuthService.API/Program.cs
+++ for-self/AuthService.API/Program.cs
@@ -11,8 +11,6 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddSingleton<List<UserResponse>>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,54 +25,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapPost("/signup", SignUp);
-
 app.Run();
 
-static IResult SignUp(RegisterRequest request, List<UserResponse> users)
-{
-    try
-    {
-        var response = UserResponse.Create(request.Email, request.Username);
 
-        users.Add(response);
 
-        return TypedResults.Created($"/users/{response.Id}", response);
-    }
-    catch (ArgumentNullException ex)
-    {
-        return TypedResults.BadRequest(ex.Message);
-    }
-    catch (Exception ex)
-    {
-        return TypedResults.BadRequest(ex.Message);
-    }
-    finally
-    {
-        Console.WriteLine("Signup attempt completed.");
-    }
-}
 
-public record RegisterRequest(string Email, string Password,string Username);
-public record UserResponse
-{
-    private UserResponse(string email, string username)
-    {
-        if (string.IsNullOrWhiteSpace(email))
-            throw new ArgumentNullException("Email must be provided.", nameof(email));
 
-        if (string.IsNullOrWhiteSpace(username))
-            throw new ArgumentNullException("Username must be provided.", nameof(username));
 
-        Id = Guid.NewGuid();
-        Email = email;
-        Username = username;
-    }
-
-    public Guid Id { get; init; }
-    public string Email { get; init; }
-    public string Username { get; init; }
-
-    public static UserResponse Create(string email, string username)
-        => new UserResponse( email, username);
-}
