8bffdaf16ae32ff17241f13a267c2f199a7655e5
rezatajari <reza.tajari70@gmail.com>
2026-04-09 11:27:39 +0330

    refactor: add email and usercontroller created

---
+++ for-self/AuthService.API/Controller/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
--- for-self/AuthService.API/Models/User.cs
+++ for-self/AuthService.API/Models/User.cs
@@ -2,19 +2,18 @@
 {
     public class User
     {
-        private User(string username,string password)
+        public User(string username,string email,string password)
         {
-            Username=username;
-            Password=password;
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentNullException("username");
+            if (string.IsNullOrWhiteSpace(email))  throw new ArgumentNullException("email");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException("password");
+            this.Username = username;
+            this.Email = email;
+            this.Password = password;
         }
 
         public string Username { get; private set; }
+        public string Email { get; private set; }
         public string Password { get; private set; }
-
-
-        public static User Create(string username,string password)
-        {
-            return new User(username,password);
-        }
     }
 }
