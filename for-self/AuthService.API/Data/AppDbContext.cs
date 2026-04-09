418f7bb62885f202e8b31d3d39de5f2d00e33c5a
rezatajari <reza.tajari70@gmail.com>
2026-04-09 16:39:32 +0330

    feat: add database connection

---
+++ for-self/AuthService.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthService.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.API.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- for-self/AuthService.API/Program.cs
+++ for-self/AuthService.API/Program.cs
@@ -1,5 +1,7 @@
+using AuthService.API.Data;
 using Microsoft.AspNetCore.Components.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Net.Sockets;
 using System.Reflection.Metadata;
 
@@ -10,6 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
