88d06e08b306a339e14bbca1224a57f25fc8f94c
rezatajari <reza.tajari70@gmail.com>
2026-04-09 23:16:49 +0330

    feat: add into database succesful

--- for-self/AuthService.API/Controller/UsersController.cs
+++ for-self/AuthService.API/Controller/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using AuthService.API.Models;
+﻿using AuthService.API.Data;
+using AuthService.API.Models;
 using AuthService.API.Shared;
 using AuthService.API.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -9,17 +10,23 @@ namespace AuthService.API.Controller
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private static List<User> users = new List<User>();
+        private readonly AppDbContext _database;
+
+        public UsersController(AppDbContext database)
+        {
+            _database = database;
+        }
 
         [HttpPost("registration")]
         public IActionResult Registration(User user)
         {
-            if (users.Any(u => u.Email == user.Email))
+            if (_database.Users.Any(u => u.Email == user.Email))
                 return BadRequest(ReturnResponse<string>.Failure(Message.Create("Email already exists")));
-            if (users.Any(u => u.Username == user.Username))
+            if (_database.Users.Any(u => u.Username == user.Username))
                 return BadRequest(ReturnResponse<string>.Failure(Message.Create("Username already exists")));
 
-            users.Add(user);
+            _database.Users.Add(user);
+            _database.SaveChanges();
             return Ok(ReturnResponse<string>.Success($"registered {user.Email}", Message.Create("User registered successfully")));
         }
 
@@ -29,7 +36,7 @@ namespace AuthService.API.Controller
             if (login == null)
                 return BadRequest(ReturnResponse<LoginResponse>.Failure(Message.Create("Your login model should not null")));
 
-            var user = users.FirstOrDefault(
+            var user = _database.Users.FirstOrDefault(
                 u => (u.Email == login.email || u.Username == login.username) 
                 && u.Password == login.password);
             if (user == null)
@@ -45,7 +52,7 @@ namespace AuthService.API.Controller
             if (changePassword == null)
                 return BadRequest(ReturnResponse<bool>.Failure(Message.Create("Your change password model should not null")));
 
-            var user=users.SingleOrDefault(u => u.Email == changePassword.Email && u.Password == changePassword.OldPassword);
+            var user=_database.Users.SingleOrDefault(u => u.Email == changePassword.Email && u.Password == changePassword.OldPassword);
             if (user == null)
                 return BadRequest(ReturnResponse<bool>.Failure(Message.Create("Invalid email or old password")));
 
@@ -59,7 +66,7 @@ namespace AuthService.API.Controller
         [HttpPost("get-all")]
         public IActionResult GetAll() { 
 
-            var result=users.Select(u=>new GetAllResponse(u.Email,u.Username,u.Id)).ToList();
+            var result=_database.Users.Select(u=>new GetAllResponse(u.Email,u.Username,u.Id)).ToList();
 
             if (result.Count == 0)
                 return BadRequest(ReturnResponse<List<GetAllResponse>>.Failure(Message.Create("No users found")));
