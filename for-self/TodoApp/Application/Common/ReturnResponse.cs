cc08103b2e5c4904ae0d3b548b362c884984a975
rezatajari <reza.tajari70@gmail.com>
2026-04-12 20:38:48 +0330

    feat: add ReturnResponse

---
+++ for-self/TodoApp/Application/Common/ReturnResponse.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Application.Common
+{
+    namespace TodoApp.Application.Common.Responses
+    {
+        public class ReturnResponse<T>
+        {
+            public bool Success { get; private set; }
+            public string? Message { get; private set; }
+            public T? Data { get; private set; }
+            public List<string>? Errors { get; private set; }
+
+            private ReturnResponse() { }
+
+            public static ReturnResponse<T> Ok(T data, string? message = null)
+            {
+                return new ReturnResponse<T>
+                {
+                    Success = true,
+                    Data = data,
+                    Message = message,
+                    Errors = null
+                };
+            }
+
+            public static ReturnResponse<T> Fail(string message, List<string>? errors = null)
+            {
+                return new ReturnResponse<T>
+                {
+                    Success = false,
+                    Data = default,
+                    Message = message,
+                    Errors = errors
+                };
+            }
+
+            public static ReturnResponse<T> Fail(List<string> errors, string? message = null)
+            {
+                return new ReturnResponse<T>
+                {
+                    Success = false,
+                    Data = default,
+                    Message = message,
+                    Errors = errors
+                };
+            }
+        }
+    }
+}
