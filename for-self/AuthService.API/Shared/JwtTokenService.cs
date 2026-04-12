5b2520d07aa3c614fca5a122bec963e43e344a3b
rezatajari <reza.tajari70@gmail.com>
2026-04-12 15:32:28 +0330

    feat: Generate jwt Token meckanism

---
+++ for-self/AuthService.API/Shared/JwtTokenService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthService.API.Shared
+{
+    public class JwtTokenService
+    {
+        private string Key;
+        private string Issuer;
+        private string Audience;
+        private int ExpiryMinutes;
+
+        public JwtTokenService(IConfiguration config)
+        {
+            Key = config["Jwt:Key"]!;
+            Issuer = config["Jwt:Issuer"]!;
+            Audience = config["Jwt:Audience"]!;
+            ExpiryMinutes = int.Parse(config["Jwt:ExpiryMinutes"]!);
+        }
+
+
+        public string GenerateToken(Guid userId,string email,string userName)
+        {
+            var claims = new[]
+            {
+                new Claim("userId",userId.ToString()),
+                new Claim("email",email),
+                new Claim("username",userName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key));
+            var credentials=new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: Issuer,
+                audience: Audience,
+                claims:claims,
+                expires:DateTime.UtcNow.AddMinutes(ExpiryMinutes),
+                signingCredentials:credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
