62b24e3723d2489b6c11c238a5970fc284f73d1b
rezatajari <reza.tajari70@gmail.com>
2026-04-12 21:28:39 +0330

    refactor: add repo into gettodos

---
+++ for-self/TodoApp/Application/Interfaces/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface ITodoRepository
+    {
+        TodoItem? GetById(Guid id);
+        void Add(TodoItem todo);
+        List<TodoItem> GetAll();
+    }
+}
--- for-self/TodoApp/Application/UseCases/GetTodo/GetTodosHandler.cs
+++ for-self/TodoApp/Application/UseCases/GetTodo/GetTodosHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Common.TodoApp.Application.Common.Responses;
+using Application.Interfaces;
 using Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,19 @@ namespace Application.UseCases.GetTodo
 {
     public class GetTodosHandler
     {
-        private readonly List<TodoItem> _todoItems=new List<TodoItem>();
+        private readonly ITodoRepository _repo;
+
+        public GetTodosHandler(ITodoRepository repo)
+        {
+            _repo = repo;
+        }
 
         public ReturnResponse<TodoItem> Handle(Guid id)
         {
-            var result=_todoItems.FirstOrDefault(x => x.Id == id);
+            var result=_repo.GetById(id);
 
             if (result == null)
-            {
                 return ReturnResponse<TodoItem>.Fail("Todo item not found");
-            }
 
             return ReturnResponse<TodoItem>.Ok(result);
         }
