84a930a730744d007156bf1a4ddbc606519b584d
rezatajari <reza.tajari70@gmail.com>
2026-04-12 21:24:12 +0330

    feat: get todos handler

--- for-self/TodoApp/Application/UseCases/GetTodo/GetTodosHandler.cs
+++ for-self/TodoApp/Application/UseCases/GetTodo/GetTodosHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Common.TodoApp.Application.Common.Responses;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,19 @@ namespace Application.UseCases.GetTodo
 {
     public class GetTodosHandler
     {
+        private readonly List<TodoItem> _todoItems=new List<TodoItem>();
+
+        public ReturnResponse<TodoItem> Handle(Guid id)
+        {
+            var result=_todoItems.FirstOrDefault(x => x.Id == id);
+
+            if (result == null)
+            {
+                return ReturnResponse<TodoItem>.Fail("Todo item not found");
+            }
+
+            return ReturnResponse<TodoItem>.Ok(result);
+        }
+
     }
 }
