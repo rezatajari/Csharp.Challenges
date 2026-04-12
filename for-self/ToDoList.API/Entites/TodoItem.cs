020a0eea156de4f85679e969b168729df687bf65
rezatajari <reza.tajari70@gmail.com>
2026-04-12 20:09:05 +0330

    feat: ReturnResponse

---
+++ for-self/ToDoList.API/Entites/TodoItem.cs
@@ -0,0 +1,33 @@
+﻿using ToDoList.API.Shared;
+
+namespace ToDoList.API.Entites
+{
+    public class TodoItem
+    {
+        public int Id { get; private set; }
+        public string Title { get;private set; }
+        public bool IsCompleted { get; private set; }
+
+        private TodoItem(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+               throw new ArgumentException("Title cannot be empty.",nameof(title));
+        }
+
+        public static TodoItem Create(string title)
+        {
+            return new TodoItem(title);
+        }
+
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+
+}
---
+++ for-self/ToDoList.API/Shared/ReturnResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace ToDoList.API.Shared
+{
+    public class ReturnResponse<T>
+    {
+        public T? Data { get; private set; }
+        public bool IsSuccess { get; private set; }
+        public string? Message { get;private set; }
+
+        private ReturnResponse(bool isSuccess, T? data,string? message)
+        {
+            IsSuccess = isSuccess;
+            Data = data;
+            Message = message;
+        }
+
+        public static ReturnResponse<T> Success(T? data,string? message)
+            => new ReturnResponse<T>(true,data,message);
+
+        public static ReturnResponse<T> Failure(string? message)
+            => new ReturnResponse<T>(false, default, message);
+    }
+}
