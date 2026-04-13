1f890fa7c942d54b0e20cb9e13d7e27c81cb9cec
rezatajari <reza.tajari70@gmail.com>
2026-04-13 10:07:01 +0330

    feat: shortlink

--- for-self/URLShortener/Entities/ShortLink.cs
+++ for-self/URLShortener/Entities/ShortLink.cs
@@ -2,41 +2,24 @@
 {
     public class ShortLink
     {
+        public string Code { get; private set; }
+        public string Url { get; private set; }
 
-        private static readonly char[] Alphabet=
-                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
-
-        private static readonly Random Random = new Random();
-
-        private readonly List<string> Codes = new List<string>();
-
-        public string Code { get;private set; }
-        public string Url { get; set; }
-
-        public string GenerateCode() {
-            while (true) {
-
-                Code = CreateRandomCode();
-
-                Codes.Add(Code);
-
-                if (!Codes.Any(c => c == Code))
-                    return Code;
-            }
-        }
-
-         string CreateRandomCode(int length=6)
+        private ShortLink(string code, string url)
         {
-            var result=new char[length];
-
-            for(int i = 0; i < length; i++)
-            {
-                result[i] = Alphabet[Random.Next(Alphabet.Length)];
-            }
-
-            return new string(result);
+            Code = code;
+            Url = url;
         }
 
-        public void CreateShortLink(string Url) { }
+        public static ShortLink Create(string code, string url)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+                throw new ArgumentException("Code is required");
+
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentException("Url is required");
+
+            return new ShortLink(code, url);
+        }
     }
 }
