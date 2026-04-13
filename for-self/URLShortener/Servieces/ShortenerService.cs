d8a26933cf66998bd1353721ad530afa9f571b41
rezatajari <reza.tajari70@gmail.com>
2026-04-13 10:09:00 +0330

    feat: ShortenerService

--- for-self/URLShortener/Servieces/ShortenerService.cs
+++ for-self/URLShortener/Servieces/ShortenerService.cs
@@ -1,6 +1,41 @@
-﻿namespace URLShortener.Servieces
+﻿using URLShortener.Entities;
+
+namespace URLShortener.Servieces
 {
     public class ShortenerService
     {
+        private readonly Dictionary<string, ShortLink> _store = new();
+
+        public ShortLink Create(string url)
+        {
+            var code = GenerateCode();
+
+            var shortLink = ShortLink.Create(code, url);
+
+            _store.Add(code, shortLink);
+
+            return shortLink;
+        }
+
+        public string? Resolve(string code)
+        {
+            return _store.TryGetValue(code, out var link)
+                ? link.Url
+                : null;
+        }
+
+        private string GenerateCode()
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+
+            var result = new char[6];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(result);
+        }
     }
-}
