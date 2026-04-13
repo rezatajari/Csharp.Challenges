578f2d04b69ad6746774d4cc1875d67f7807257f
rezatajari <reza.tajari70@gmail.com>
2026-04-13 16:30:03 +0330

    feat: add JwtTokenService

---
+++ for-self/HabitTracker.Api/Shared/JwtTokenService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HabitTracker.Api.Shared
+{
+    public class JwtTokenService
+    {
+        private string Key;
+        private string Issuer;
+        private string Audience;
+        private int ExpiryMinutes;
+
+        public JwtTokenService(IConfiguration config)
+        {
+            Key = config["Jwt:Key"]!;
+            Issuer = config["Jwt:Issuer"]!;
+            Audience = config["Jwt:Audience"]!;
+            ExpiryMinutes = int.Parse(config["Jwt:ExpiryMinutes"]!);
+        }
+
+        public string GenerateToken(Guid userId, string email, string userName)
+        {
+            var claims = new[]
+            {
+                new Claim("userId",userId.ToString()),
+                new Claim("email",email),
+                new Claim("username",userName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: Issuer,
+                audience: Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(ExpiryMinutes),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
