5441ab6fe0cf327c2d9f181cb0125c1165a549b5
rezatajari <reza.tajari70@gmail.com>
2026-04-14 09:24:31 +0330

    feat: OnModelCreating for account

--- Programming-C#-12-(Ian Griffiths)/FinanceTracker/Data/FinanceDbContext.cs
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Data/FinanceDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FinanceTracker.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +8,23 @@ namespace FinanceTracker.Data
 {
     public class FinanceDbContext:DbContext
     {
-        public FinanceDbContext( DbContextOptions<FinanceDbContext> options)
+        public FinanceDbContext(DbContextOptions<FinanceDbContext> options)
             :base(options){}
+
+        public DbSet<Account> Accounts{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(a => a.Id);
+
+                entity.OwnsOne(a => a.Balance, balance =>
+                {
+                    balance.Property(m => m.Amount).HasColumnName("BalanceAcmount").HasPrecision(18, 2);
+                    balance.Property(m => m.Currency).HasColumnName("BalanceCurrency").HasConversion<string>();
+                });
+            });
+        }
     }
 }
