541af470cc8889d1e7d4e2fb6d62908bb7bec6e6
rezatajari <reza.tajari70@gmail.com>
2026-04-15 12:32:46 +0330

    feat: create BaseRepository

---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using FinanceTracker.Entities;
+using FinanceTracker.Interfaces;
+
+namespace FinanceTracker.Data.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+    }
+}
