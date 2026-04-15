aea6b14eb3f0eed41124664548f49c376cef3ce8
rezatajari <reza.tajari70@gmail.com>
2026-04-15 20:18:28 +0330

    feat: add childsTransactions

--- Programming-C#-12-(Ian Griffiths)/FinanceTracker/Migrations/20260415164731_childsTransactions.Designer.cs
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Migrations/20260415164731_childsTransactions.Designer.cs
@@ -4,6 +4,7 @@ using FinanceTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinanceTracker.Migrations
 {
     [DbContext(typeof(FinanceDbContext))]
-    partial class FinanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260415164731_childsTransactions")]
+    partial class childsTransactions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Migrations/20260415164731_childsTransactions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class childsTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Transactions",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ToAccountId",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ToAccountId",
+                table: "Transactions",
+                column: "ToAccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_BaseAccounts_ToAccountId",
+                table: "Transactions",
+                column: "ToAccountId",
+                principalTable: "BaseAccounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_BaseAccounts_ToAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_ToAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ToAccountId",
+                table: "Transactions");
+        }
+    }
+}
