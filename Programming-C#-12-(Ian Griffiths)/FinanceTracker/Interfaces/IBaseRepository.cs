8fbfba0b17fdf19ebf9142a3ef9e723594c92469
rezatajari <reza.tajari70@gmail.com>
2026-04-15 12:19:06 +0330

    refactor: IBaseRepository as sync vs async

--- Programming-C#-12-(Ian Griffiths)/FinanceTracker/Interfaces/IBaseRepository.cs
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Interfaces/IBaseRepository.cs
@@ -4,10 +4,11 @@ namespace FinanceTracker.Interfaces
 {
     public interface IBaseRepository<T> where T:BaseEntity
     {
-        Task<T> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
-        Task AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
+        void AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<int> SaveChangesAsync();
     }
 }
