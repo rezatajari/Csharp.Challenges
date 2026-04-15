c247bbe7424236de7d6ff9587e55e12d194d6540
rezatajari <reza.tajari70@gmail.com>
2026-04-15 12:01:54 +0330

    feat: abstract class BaseEntity

---
+++ Programming-C#-12-(Ian Griffiths)/FinanceTracker/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get;protected set; }
+        public DateTime CreatedAt { get;protected set; } = DateTime.Now;
+        public bool IsDeleted { get; protected set; }
+    }
+}
