38e875b2acf4b90fb2ffedd0392e60df12b697c6
rezatajari <reza.tajari70@gmail.com>
2026-04-17 19:40:54 +0330

    refactor: CreateTodoHandler as async

--- for-self/TodoApp/Application/UseCases/CreateTodo/CreateTodoHandler.cs
+++ for-self/TodoApp/Application/UseCases/CreateTodo/CreateTodoHandler.cs
@@ -1,25 +1,28 @@
 ﻿using Application.Common.TodoApp.Application.Common.Responses;
-using Application.DTOs
+using Application.DTOs;
+using Application.Interfaces;
 using Domain.Entities;
+using System.Threading.Tasks;
 
 namespace Application.UseCases.CreateTodo
 {
     public class CreateTodoHandler
     {
-        private readonly ITodoRepository _repo;
-        public CreateTodoHandler(ITodoRepository repo)
+        private readonly ITodoRepository _todoRepo;
+        public CreateTodoHandler(ITodoRepository todoRepo)
         {
-            _repo = repo;
+            _todoRepo = todoRepo;
         }
 
-        public ReturnResponse<TodoItem> Handle(CreateTodoDto request)
+        public async Task<ReturnResponse<TodoItem>> Handle(CreateTodoDto request)
         {
             if (string.IsNullOrWhiteSpace(request.Title))
                 return ReturnResponse<TodoItem>.Fail("Title cannot be empty.");
 
             var todo = TodoItem.Create(request.Title);
 
-            _repo.Add(todo);
+            await _todoRepo.AddAsync(todo);
+            await _todoRepo.SaveChangesAsync();
 
             return ReturnResponse<TodoItem>.Ok(todo, "Todo item created successfully.");
         }
