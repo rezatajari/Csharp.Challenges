5c69416307a86cbb681bdd21b05f704ee49f87f7
rezatajari <reza.tajari70@gmail.com>
2026-04-21 14:31:22 +0330

    feat: CreateAccount in AccountService from UI side

--- for-self/FinanceTracker/UI/Services/AccountService.cs
+++ for-self/FinanceTracker/UI/Services/AccountService.cs
@@ -1,4 +1,9 @@
-﻿namespace UI.Services
+﻿using Application.Dtos;
+using Domain.Shared;
+using System.Net.Http.Json;
+using UI.Pages;
+
+namespace UI.Services
 {
     public class AccountService
     {
@@ -6,6 +11,14 @@
         public AccountService(HttpClient client)
         {
             _client = client;
+        }
+
+        public async Task<ApiResult<bool>> CreateAccount(CreateAccountDto dto)
+        {
+            var response = await _client.PostAsJsonAsync("api/Finance/create-account", dto);
+
+            return await response.Content.ReadFromJsonAsync<ApiResult<bool>>()
+                ?? ApiResult<bool>.Failure("Error connect to server");
         }
     }
 }
