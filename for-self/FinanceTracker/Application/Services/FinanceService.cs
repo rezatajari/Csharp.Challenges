417f912c7c263b3529e65368f64f1cd80d73f9f3
rezatajari <reza.tajari70@gmail.com>
2026-04-21 14:11:14 +0330

    refactor: IncomeTxDto as new model

--- for-self/FinanceTracker/Application/Services/FinanceService.cs
+++ for-self/FinanceTracker/Application/Services/FinanceService.cs
@@ -18,25 +18,36 @@ namespace Application.Services
 
         public async Task<Result<bool>> OpenAccount(CreateAccountDto createAccDto)
         {
-            if (createAccDto.Type==AccountType.Savings)
-                SavingsAccount.Create(createAccDto.Name, createAccDto.Balance, createAccDto.Type);
-            if (createAccDto.Type == AccountType.CreditCard)
-                CreditCardAccount.Create(createAccDto.Name, createAccDto.Balance, createAccDto.CreditLimit);
-            await _accountRepo.AddAsync(newAccount);
+            BaseAccount? newAccount = null;
 
-            return (await _accountRepo.SaveChangesAsync()>0)
+            if (createAccDto.Type == AccountType.Savings)
+            {
+                newAccount = SavingsAccount.Create(createAccDto.Name, createAccDto.Balance,createAccDto.Type);
+            }
+            else if (createAccDto.Type == AccountType.CreditCard)
+            {
+                newAccount = CreditCardAccount.Create(createAccDto.Name, createAccDto.Balance, createAccDto.CreditLimit,createAccDto.Type);
+            }
+
+            if (newAccount == null)
+                return Result<bool>.Failure("The type of account is not exist");
+
+            await _accountRepo.AddAsync(newAccount);
+            var success = await _accountRepo.SaveChangesAsync() > 0;
+
+            return (success)
                 ? Result<bool>.Success(true)
                 : Result<bool>.Failure("Failed to open account.");
         }
 
         public async Task<Result<bool>> RecordIncome(InputRecordTxDto IncomeTxDto)
         {
-            var account = await _accountRepo.GetByIdAsync(IncomeTx.accountId);
+            var account = await _accountRepo.GetByIdAsync(IncomeTxDto.accountId);
             if (account == null)
                 return Result<bool>.Failure("Account not found.");
 
-            account.Deposit(IncomeTx.amount, IncomeTx.category,
-                IncomeTx.description, DateTime.UtcNow);
+            account.Deposit(IncomeTxDto.amount, IncomeTxDto.category,
+                IncomeTxDto.description, DateTime.UtcNow);
 
             return (await _accountRepo.SaveChangesAsync() > 0)
                 ? Result<bool>.Success(true)
@@ -52,6 +63,6 @@ namespace Application.Services
         {
             throw new NotImplementedException();
         }
-   
+
     }
 }
