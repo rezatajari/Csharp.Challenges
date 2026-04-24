e3469c8808db7cf9d3ff8c1e93d51c00307ea534
rezatajari <reza.tajari70@gmail.com>
2026-04-24 07:41:25 +0330

    Squashed 'application-projects/LibraryAppMVC/' content from commit 0c3cf19
    git-subtree-dir: application-projects/LibraryAppMVC
    git-subtree-split: 0c3cf192d341f6c6d9195c6d49cb68248e84aa5e

---
+++ Domain/Abstractions/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects.Book;
+
+namespace Domain.Abstractions
+{
+    public interface IBookRepository
+    {
+        Task<Book?> GetByIdAsync(Isbn id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+    }
+}
