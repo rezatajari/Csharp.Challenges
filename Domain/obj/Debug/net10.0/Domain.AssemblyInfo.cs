0c3cf192d341f6c6d9195c6d49cb68248e84aa5e
rezatajari <reza.tajari70@gmail.com>
2026-04-24 07:28:05 +0330

    chore: some change

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93d70a7148d967b974055d823b2746c6258009a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c322270e04214f09ed9e0cc2f40070306c215d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
