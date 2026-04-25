ab8cddb67b74a8be0d77d9185a91fd50c3dbd45e
rezatajari <reza.tajari70@gmail.com>
2026-04-25 13:27:29 +0330

    refactor: add CancellationToken  in a GetAccountWithTransactionsAsync

--- application-projects/FinanceTracker/Infrastructure/Repositories/FinanceRepository.cs
+++ application-projects/FinanceTracker/Infrastructure/Repositories/FinanceRepository.cs
@@ -8,9 +8,9 @@ namespace Infrastructure.Repositories
     public class FinanceRepository : BaseRepository<BaseAccount>, IFinanceRepository
     {
         public FinanceRepository(FinanceDbContext context) : base(context) { }
-        public async Task<BaseAccount?> GetAccountWithTransactionsAsync(int id)
+        public async Task<BaseAccount?> GetAccountWithTransactionsAsync(int id,CancellationToken ct)
         {
-            return await _dbSet.Include(a=>a.Transactions).FirstOrDefaultAsync(a => a.Id == id);
+            return await _dbSet.Include(a=>a.Transactions).FirstOrDefaultAsync(a => a.Id == id,ct);
         }
     }
 }
