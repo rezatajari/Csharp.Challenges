aff4dcdc499cdb834508d01f695d221cb78e262c
rezatajari <reza.tajari70@gmail.com>
2026-04-25 14:43:38 +0330

    feat: add contoller logs

--- application-projects/FinanceTracker/Api/Controllers/FinanceController.cs
+++ application-projects/FinanceTracker/Api/Controllers/FinanceController.cs
@@ -1,53 +1,51 @@
 ﻿using Application.Dtos;
 using Application.Interfaces;
-using Domain.Shared;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class FinanceController : ApiControllerBase
+    public class FinanceController(IFinanceService financeService, ILogger<FinanceController> logger) : ApiControllerBase
     {
-        private readonly IFinanceService _financeService;
-        public FinanceController(IFinanceService financeService)
-        {
-            _financeService = financeService;
-        }
-
         [HttpPost("create-account")]
-        public async Task<IActionResult> CreateAccount(CreateAccountDto createAccModel,CancellationToken ct)
+        public async Task<IActionResult> CreateAccount(CreateAccountDto createAccModel, CancellationToken ct)
         {
-            var result = await _financeService.OpenAccount(createAccModel,ct);
+            logger.LogInformation("Received request to create account: {AccountName}", createAccModel.Name);
+            var result = await financeService.OpenAccount(createAccModel, ct);
             return HandleResult(result);
         }
 
         [HttpGet("accounts")]
         public async Task<IActionResult> GetAllAccounts(CancellationToken ct)
         {
-            var result = await _financeService.GetAccounts(ct);
+            logger.LogInformation("Received request to fetch all accounts");
+            var result = await financeService.GetAccounts(ct);
             return HandleResult(result);
         }
 
         [HttpGet("account/{id}")]
         public async Task<IActionResult> GetAccount(int id, CancellationToken ct)
         {
-            var result = await _financeService.GetAccount(id,ct);
+            logger.LogInformation("Received request for Account Details. Id: {AccountId}", id);
+            var result = await financeService.GetAccount(id, ct);
             return HandleResult(result);
         }
 
         [HttpGet("transaction/{id}")]
         public async Task<IActionResult> GetTransaction(int id, CancellationToken ct)
         {
-            var result = await _financeService.GetAccountTransactions(id,ct);
+            logger.LogInformation("Received request for Transactions. AccountId: {AccountId}", id);
+            var result = await financeService.GetAccountTransactions(id, ct);
             return HandleResult(result);
         }
 
         [HttpPost("transaction/add")]
         public async Task<IActionResult> AddTransaction(InputTxDto inputTxDto, CancellationToken ct)
         {
-            var result = await _financeService.AddTransaction(inputTxDto,ct);
+            logger.LogInformation("Received request to add {TransactionType} for AccountId: {AccountId}",
+                inputTxDto.transactionType, inputTxDto.accountId);
+            var result = await financeService.AddTransaction(inputTxDto, ct);
             return HandleResult(result);
         }
     }
