740d0abe1d192c6af7ab9ca1cd5bbc2971ffac21
rezatajari <reza.tajari70@gmail.com>
2026-04-27 13:51:24 +0330

    feat: create AuthController

---
+++ application-projects/FinanceTracker/Api/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ApiControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(CreateUserRequest request)
+        {
+
+        }
+    }
+}
