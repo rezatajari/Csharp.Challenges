02c3baf47c0df595aa4fc42f08ea9e3f94c06376
rezatajari <reza.tajari70@gmail.com>
2026-04-27 12:39:15 +0330

    feat: JwtProvider

---
+++ application-projects/FinanceTracker/Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Application.Shared;
+using Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Authentication
+{
+    public class JwtProvider(IOptions<JwtSettings> settings) : IJwtProvider
+    {
+        private readonly JwtSettings _settings = settings.Value;
+
+        public string Generate(User user)
+        {
+            var claims = new Claim[]
+            {
+                new (JwtRegisteredClaimNames.Sub,user.Id.ToString()),
+                new (JwtRegisteredClaimNames.Email,user.Email.ToString()),
+                new ("Username",user.Username)
+            };
+
+            var key=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.Key));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token =
+                new JwtSecurityToken(_settings.Issuer, _settings.Audience, claims,
+                    null, DateTime.UtcNow.AddMinutes(_settings.DurationInMinutes), creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
