e4bdba045ad187ecad73925e0b31be89c0d8fbc5
rezatajari <reza.tajari70@gmail.com>
2026-04-29 14:44:12 +0330

    feat: BackgroundTaskQueue and IBackgroundTaskQueue

---
+++ application-projects/FinanceTracker/Infrastructure/BackgroundJobs/BackgroundTaskQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Infrastructure.BackgroundJobs
+{
+    public interface IBackgroundTaskQueue
+    {
+        ValueTask QueueBackgroundWorkItemAsync(string workItem);
+        ValueTask<string> DequeueAsync(CancellationToken cancellationToken);
+    }
+    public class BackgroundTaskQueue: IBackgroundTaskQueue
+    {
+        private readonly Channel<string> _queue;
+        public BackgroundTaskQueue()
+        {
+            var option = new BoundedChannelOptions(100)
+            {
+                FullMode = BoundedChannelFullMode.Wait
+            };
+            _queue= Channel.CreateBounded<string>(option);
+        }
+        public ValueTask<string> DequeueAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask QueueBackgroundWorkItemAsync(string workItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
