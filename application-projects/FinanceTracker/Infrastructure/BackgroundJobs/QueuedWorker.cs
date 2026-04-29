9ae61e2b28b5608e7788cbc215de8257eea88723
rezatajari <reza.tajari70@gmail.com>
2026-04-29 15:07:09 +0330

    feat: implement QueuedWorker

--- application-projects/FinanceTracker/Infrastructure/BackgroundJobs/QueuedWorker.cs
+++ application-projects/FinanceTracker/Infrastructure/BackgroundJobs/QueuedWorker.cs
@@ -6,11 +6,18 @@ using System.Text;
 
 namespace Infrastructure.BackgroundJobs
 {
-    public class QueuedWorker(ILogger<QueuedWorker> logger) : BackgroundService
+    public class QueuedWorker(ILogger<QueuedWorker> logger,IBackgroundTaskQueue taskQueue) : BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             logger.LogInformation("Queued worker is waiting for jobs...");
+
+            while (!stoppingToken.IsCancellationRequested) { 
+                var workItem=await taskQueue.DequeueAsync(stoppingToken);
+                logger.LogInformation("Worker grabbed job: {job}. Starting processing...", workItem);
+                await Task.Delay(5000, stoppingToken);
+                logger.LogInformation("Finished job: {job}", workItem);
+            }
         }
     }
 }
