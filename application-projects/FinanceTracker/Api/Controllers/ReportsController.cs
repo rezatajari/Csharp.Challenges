c88354432aea94a166c29afc48e6ceccc7b18eee
rezatajari <reza.tajari70@gmail.com>
2026-04-29 15:00:33 +0330

    feat: RequestReport in ReportsController

---
+++ application-projects/FinanceTracker/Api/Controllers/ReportsController.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.BackgroundJobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController(IBackgroundTaskQueue taskQueue) : ApiControllerBase
+    {
+        [HttpPost("request-pdf")]
+        public async Task<IActionResult> RequestReport(string reportName)
+        {
+            await taskQueue.QueueBackgroundWorkItemAsync($"Generating PDF for: {reportName}");
+            return Accepted(new { Message = "Report is being processed in the background." });
+        }
+    }
+}
