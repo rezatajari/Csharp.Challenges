a38a79c76522af952347f097a2a081f0d4abd2e0
rezatajari <reza.tajari70@gmail.com>
2026-04-29 12:24:53 +0330

    feat: backgroud job test

---
+++ application-projects/FinanceTracker/Infrastructure/BackgroundJobs/MonthlyReportWorker.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.BackgroundJobs
+{
+    public class MonthlyReportWorker(ILogger<MonthlyReportWorker> logger) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("Midnight Janitor: Cheking for reports to generate at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
