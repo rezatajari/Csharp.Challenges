e322ed9e8c0c2a61c6031bc05ff58f93e97f336e
rezatajari <reza.tajari70@gmail.com>
2026-04-29 13:19:29 +0330

    feat: use using {} and implement a test background job

--- application-projects/FinanceTracker/Infrastructure/BackgroundJobs/MonthlyReportWorker.cs
+++ application-projects/FinanceTracker/Infrastructure/BackgroundJobs/MonthlyReportWorker.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.BackgroundJobs
 {
-    public class MonthlyReportWorker(ILogger<MonthlyReportWorker> logger) : BackgroundService
+    public class MonthlyReportWorker(IServiceScopeFactory _serviceScopeFactory, ILogger<MonthlyReportWorker> logger) : BackgroundService
     {
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
-            while (!stoppingToken.IsCancellationRequested)
+            using (IServiceScope scope = _serviceScopeFactory.CreateScope())
             {
-                logger.LogInformation("Midnight Janitor: Cheking for reports to generate at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<FinanceDbContext>();
+
+                    var count=dbContext.Transactions.Count();
+                    logger.LogInformation("Midnight Janitor: Cheking for reports to generate at: {time}", DateTimeOffset.Now);
+                    await Task.Delay(1000, stoppingToken);
+                }
             }
         }
     }
