aadb5e7cf0910997cd01652036e756805705d4de
rezatajari <reza.tajari70@gmail.com>
2026-05-01 13:31:01 +0330

    feat: RegisterUserForm

---
+++ application-projects/FinanceTracker/UI/Models/RegisterUserForm.cs
@@ -0,0 +1,10 @@
+﻿namespace UI.Models
+{
+    public class RegisterUserForm
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } 
+        public string ConfirmPassword { get; set; } 
+    }
+}
