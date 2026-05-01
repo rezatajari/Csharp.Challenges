896945bce3b0c6c5b8d494bbd4ee23a2d35ff2aa
rezatajari <reza.tajari70@gmail.com>
2026-05-01 17:14:48 +0330

    feat: DashboardResponse

---
+++ application-projects/FinanceTracker/Application/Dtos/Reponses/DashboardResponse.cs
@@ -0,0 +1,7 @@
+﻿using Domain.ValueObjects;
+
+namespace Application.Dtos.Reponses
+{
+    public record DashboardResponse(List<DashboardAccounts> accounts);
+    public record DashboardAccounts(string AccountName,Money Balance);
+}
