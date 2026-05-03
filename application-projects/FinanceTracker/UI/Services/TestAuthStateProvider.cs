bf5874e0d299740b07af660dfcd2379ba3c3f16e
rezatajari <reza.tajari70@gmail.com>
2026-05-03 12:46:21 +0330

    feat: parsing jwt from UI

--- application-projects/FinanceTracker/UI/Services/TestAuthStateProvider.cs
+++ application-projects/FinanceTracker/UI/Services/TestAuthStateProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
+using System.Text.Json;
 
 namespace UI.Services
 {
@@ -12,11 +13,27 @@ namespace UI.Services
             if (string.IsNullOrEmpty(token))
                 return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
 
-            var identity = new ClaimsIdentity([new Claim(ClaimTypes.Name, "User")], "jwt");
+            var claims = ParseClaimsFromJwt(token);
+        }
 
-            var user = new ClaimsPrincipal(identity);
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
 
-            return new AuthenticationState(user);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()??""));
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2:base64 += "==";break;
+                case 3: base64 += "=";break;
+            }
+
+            return Convert.FromBase64String(base64);
         }
     }
 }
