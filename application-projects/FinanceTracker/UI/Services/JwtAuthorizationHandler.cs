35c4bc758009773e236767acea7bbd67f967c836
rezatajari <reza.tajari70@gmail.com>
2026-05-03 13:52:13 +0330

    refactor: implement JwtAuthorizationHandler as ILocalStorage service

--- application-projects/FinanceTracker/UI/Services/JwtAuthorizationHandler.cs
+++ application-projects/FinanceTracker/UI/Services/JwtAuthorizationHandler.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.JSInterop;
+﻿using Blazored.LocalStorage;
+using Microsoft.JSInterop;
 using System.Runtime.InteropServices;
 
 namespace UI.Services
 {
-    public class JwtAuthorizationHandler(IJSRuntime jsRuntime):DelegatingHandler
+    public class JwtAuthorizationHandler(ILocalStorageService localStorage):DelegatingHandler
     {
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var token= await jsRuntime.InvokeAsync<string>("localStorage.getItem", "authToken");
+            var token= await localStorage.GetItemAsync<string>("authToken");
             if (!string.IsNullOrEmpty(token))
                 request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
