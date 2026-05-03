fd49b4a5c3773f4b3ad1a1a9afb93340783607ce
rezatajari <reza.tajari70@gmail.com>
2026-05-03 14:35:57 +0330

    feat: CreateAccountFormModel in UI model

---
+++ application-projects/FinanceTracker/UI/Models/CreateAccountFormModel.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects;
+
+namespace UI.Models
+{
+    public class CreateAccountFormModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal InitialBalance { get; set; }
+        public Currency Currency { get; set; } = Currency.USD;
+
+        public AccountType Type { get; set; }
+
+        public decimal CreditLimitAmount { get; set; }
+    }
+}
