b38dceb9c2880e64d89b978b63f8544e9fc4d5ac
rezatajari <reza.tajari70@gmail.com>
2026-05-06 10:00:05 +0330

    refactor: shared in application

---
+++ application-projects/TodoApp/Application/Shared/DTOs/CreateTodoForm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Shared.DTOs
+{
+    public class CreateTodoForm
+    {
+        public string Title { get; set; }=string.Empty;
+    }
+}
