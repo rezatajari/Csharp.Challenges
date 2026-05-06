ad8b2db4acc677ddeb30d16cee88605b22eb3231
rezatajari <reza.tajari70@gmail.com>
2026-05-06 14:42:46 +0330

    Create ITodoRepository.cs

---
+++ application-projects/TodoApp/Application/IRepositories/ITodoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.IRepositories
+{
+    public interface ITodoRepository
+    {
+        Task<TodoItem> AddAsync(TodoItem item);
+        Task<TodoItem> GetAsync(int Id);
+        Task<List<TodoItem>> GetAllAsync();
+        Task<TodoItem> UpdateAsync(TodoItem item);
+        Task<TodoItem> DeleteAsync(int Id);
+    }
+}
