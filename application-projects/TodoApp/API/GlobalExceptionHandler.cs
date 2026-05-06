c49e1259e5fc6c74636e800d5143f7818c6e98d9
rezatajari <reza.tajari70@gmail.com>
2026-05-06 17:35:41 +0330

    refactor: global exception
    Pattern Matching

--- application-projects/TodoApp/API/GlobalExceptionHandler.cs
+++ application-projects/TodoApp/API/GlobalExceptionHandler.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Security;
 
 namespace API
 {
     public class GlobalExceptionHandler : IExceptionHandler
     {
-        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
 
+            var (statusCode, title) = exception switch
+            {
+                KeyNotFoundException => (StatusCodes.Status404NotFound, "Resource Not Found"),
+                InvalidOperationException => (StatusCodes.Status400BadRequest, "Invalid Operation"),
+                _ => (StatusCodes.Status500InternalServerError, "Server Error")
+            };
+
+            ProblemDetails problemDetails = new()
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server Error",
+                Detail = "An unexpected error occured."
+            };
+
+
+            httpContext.Response.StatusCode = problemDetails.Status.Value;
+            await httpContext.Response.WriteAsJsonAsync(problemDetails,cancellationToken);
+            return true;
         }
     }
 }
