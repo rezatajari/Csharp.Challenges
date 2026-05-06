b7d20157eb1a5c98c1c2f369fd22abddcae9d1d7
rezatajari <reza.tajari70@gmail.com>
2026-05-06 15:49:23 +0330

    feat: result<T> pattern

---
+++ application-projects/TodoApp/Application/Shared/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Shared
+{
+    public class Result<T>
+    {
+        public T? Data { get; }
+        public string? ErrorMessage { get; }
+        public bool IsSuccess => ErrorMessage == null;
+
+        private Result(T? data, string? errorMessage)
+        {
+            Data = data;
+            ErrorMessage = errorMessage;
+        }
+
+        public static Result<T> Success(T Data) => new(Data, string.Empty);
+        public static Result<T> Failure(string errorMessage) => new(default, errorMessage);
+    }
+}
