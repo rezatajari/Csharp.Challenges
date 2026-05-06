76eb523e5f5de2bdcef9273b57cbbcc3e0aa40b4
rezatajari <reza.tajari70@gmail.com>
2026-05-06 13:43:52 +0330

    feat: run database

---
+++ application-projects/TodoApp/Infrastructure/Data/TodoAppDb.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class TodoAppDb(DbContextOptions<TodoAppDb> options) : DbContext(options)
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>(t =>
+            {
+                t.HasKey(t => t.Id);
+                t.HasQueryFilter(t => !t.IsDeleted);
+            });
+        }
+    }
+}
