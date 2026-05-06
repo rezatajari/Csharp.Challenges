2ac38468dc0215abc8f66320d4f30391d1fa9832
rezatajari <reza.tajari70@gmail.com>
2026-05-06 14:44:31 +0330

    Create TodoRepository.cs

---
+++ application-projects/TodoApp/Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.IRepositories;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        public Task<TodoItem> AddAsync(TodoItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItem> DeleteAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TodoItem>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItem> GetAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItem> UpdateAsync(TodoItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
