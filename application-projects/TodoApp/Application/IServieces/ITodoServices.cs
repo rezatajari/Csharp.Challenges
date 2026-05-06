b6075133e476610c17e5e069087e36aef51c39d0
rezatajari <reza.tajari70@gmail.com>
2026-05-06 14:53:45 +0330

    feat: ITodoService

---
+++ application-projects/TodoApp/Application/IServieces/ITodoServices.cs
@@ -0,0 +1,13 @@
+﻿using Application.Shared.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.IServieces
+{
+    public interface ITodoServices
+    {
+        Task<TodoItem> CreateTodoItem(CreateTodoForm requst);
+    }
+}
--- application-projects/TodoApp/Infrastructure/Repositories/TodoRepository.cs
+++ application-projects/TodoApp/Infrastructure/Repositories/TodoRepository.cs
@@ -6,26 +6,33 @@ namespace Infrastructure.Repositories
 {
     public class TodoRepository(TodoAppDb context) : ITodoRepository
     {
-        public Task<TodoItem> AddAsync(TodoItem item)
+        public async Task AddAsync(TodoItem item, CancellationToken ct)
         {
+            await context.AddAsync(item, ct);
+            await context.SaveChangesAsync(ct);
         }
 
-        public Task<TodoItem> DeleteAsync(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<TodoItem>> GetAllAsync()
+        public Task<TodoItem> DeleteAsync(int Id, CancellationToken ct)
         {
             throw new NotImplementedException();
         }
 
-        public Task<TodoItem> GetAsync(int Id)
+        public Task<List<TodoItem>> GetAllAsync(CancellationToken ct)
         {
             throw new NotImplementedException();
         }
 
-        public Task<TodoItem> UpdateAsync(TodoItem item)
+        public Task<TodoItem> GetAsync(int Id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangeAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItem> UpdateAsync(TodoItem item, CancellationToken ct)
         {
             throw new NotImplementedException();
         }
