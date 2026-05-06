aea7b7a65ad2447dcaef2ed50ffd76e43cf6a75e
rezatajari <reza.tajari70@gmail.com>
2026-05-06 14:01:07 +0330

    feat: GetById

--- application-projects/TodoApp/API/Controllers/TodosController.cs
+++ application-projects/TodoApp/API/Controllers/TodosController.cs
@@ -2,6 +2,7 @@
 using Domain.Entities;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,5 +18,15 @@ namespace API.Controllers
             await context.SaveChangesAsync();
             return Ok(item);
         }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            TodoItem? item = await context.TodoItems.FirstOrDefaultAsync(t => t.Id == Id);
+            if (item is null) 
+                return NotFound();
+            return Ok(item);
+        }
+
     }
 }
