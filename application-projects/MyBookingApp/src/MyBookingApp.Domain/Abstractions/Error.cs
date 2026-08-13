ffe4be98adf2c134c1dec2a4d8d92eb66b7fc5ba
rezatajari <reza.tajari70@gmail.com>
2026-08-13 09:01:11 +0330

    feat: Error in duiring eppliaction class

---
+++ application-projects/MyBookingApp/src/MyBookingApp.Domain/Abstractions/Error.cs
@@ -0,0 +1,10 @@
+namespace MyBookingApp.Domain.Abstractions;
+
+
+
+public record c(string Code,string Name)
+{
+    public static readonly Error None=new (string.Empty,string.Empty);
+    
+    public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+}
---
+++ application-projects/MyBookingApp/src/MyBookingApp.Domain/Users/UserErrors.cs
@@ -0,0 +1,9 @@
+namespace MyBookingApp.Domain.Users;
+
+
+public static class UserErrors
+{
+    public static readonly UserErrors NotFound=new (
+        
+    )
+}
