1a985fc03ba43b6e2309141b7124c97910010cd4
rezatajari <reza.tajari70@gmail.com>
2026-08-13 08:57:12 +0330

    feat: add IUserRepository

---
+++ application-projects/MyBookingApp/src/MyBookingApp.Domain/Users/IUserRepository.cs
@@ -0,0 +1,9 @@
+namespace MyBookingApp.Domain.Users;
+
+
+
+public interface  IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id,CancellationToken cancellationToken=default);
+    void Add(User user);
+}
