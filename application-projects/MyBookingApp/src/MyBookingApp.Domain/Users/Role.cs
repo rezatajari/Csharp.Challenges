be1953b613d4d6b8a39ba9494011394d4e6ec600
rezatajari <reza.tajari70@gmail.com>
2026-08-13 08:08:32 +0330

    feat: role

--- application-projects/MyBookingApp/src/MyBookingApp.Domain/Users/Role.cs
+++ application-projects/MyBookingApp/src/MyBookingApp.Domain/Users/Role.cs
@@ -3,6 +3,14 @@ namespace MyBookingApp.Domain.Users;
 
 public sealed class Role
 {
-    
-    public ICollection<User> users {get;init}=new List<User>();
+    public static readonly Role Registered = new(1, "Registered");
+    public Role(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public ICollection<User> Users { get; init} = new List<User>();
+    public ICollection<Permission> Permissions { get; init; } = new List<Permission>();
 }
